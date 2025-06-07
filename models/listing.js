10cd4071204b972c6e84d2c91863dffd68a05d82
Satya Satyajit <satyajitsahoo28252@gmail.com>
2025-06-07 11:36:50 +0530

    finishing up the phase 2 part 1

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose") ;
 const Schema = mongoose.Schema ;
+const Reviews = require("./review.js") ;
 const listingSchema = new Schema({
   title: { type: String, required: true },
   description: String,
@@ -18,7 +19,13 @@ const listingSchema = new Schema({
   ]
 });
 
+listingSchema.post("findOneAndDelete" , async (listing) =>{
 
+    if(listing) {
+    await Reviews.deleteMany({_id : {$in : listing.reviews}}) ;
+    console.log("reviews also deleted") ;
+    }
+})
 
 const Listing = mongoose.model("Listing",listingSchema) ;
 module.exports = Listing ;
